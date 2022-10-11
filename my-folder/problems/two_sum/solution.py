f883a31cf1204be46b67bc868c1bbba0eb836154
Piyush <131725874+Piyush-Bhor@users.noreply.github.com>
2022-10-11 12:30:55 +0000

    Sync LeetCode submission - Two Sum (python)

--- my-folder/problems/two_sum/solution.py
+++ my-folder/problems/two_sum/solution.py
@@ -1,9 +1,18 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashTable = dict()
-        for i in range(len(nums)):
-            diff = target - nums[i]
-            if nums[i] in hashTable:
-                return(hashTable[nums[i]], i)
-            else:
-                hashTable[diff] = i
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        def twoSum2(nums,target):
+            result = []
+            for i in range(len(nums)):
+                temp = target - nums[i]
+                if temp in nums and nums.index(temp) != i:
+                    result.append(i)
+                    result.append(nums.index(temp))
+                    break
+            return result
+        return twoSum2(nums,target)
+        
